0405e43e2ef45b08c913907f9859fc93a36ab2ab
JjungminLee <julie0964@naver.com>
2022-11-02 12:57:29 +0900

    위에서 아래로

---
+++ src/이코테/정렬_위에서아래로.java
@@ -0,0 +1,48 @@
+package 이코테;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class 정렬_위에서아래로 {
+    public static int n;
+    public static int arr[];
+
+
+
+
+
+
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<n-1;i++){
+            for(int j=1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    int tmp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=tmp;
+                }
+            }
+        }
+
+
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+
+
+
+
+    }
+
+}
